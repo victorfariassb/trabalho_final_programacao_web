e0794898cec93dcf3b8fca05208d8c8f6c89ecb7
meyrele <meyrele@me.com>
2022-04-14 15:38:38 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,3 +30,14 @@ const myChart = new Chart(
     document.getElementById('myChart'),
     config
 );
+
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
